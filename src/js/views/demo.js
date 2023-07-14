d8baa85445a0b83d389678da7a982bfb3b556da6
Salomon Franco <110070841+SalomonFranco@users.noreply.github.com>
2023-07-14 22:24:15 +0000

    structure&LogicEnhanced

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -11,38 +11,104 @@ export const Demo = props => {
 	const[fullPhone, setFullPhone] = useState()
 	const[fullAddress, setFullAddress] = useState()
 
+	const handleSubmit = () => {
+		const contact = {
+			"full_name": fullName,
+			"email": fullEmail,
+			"agenda_slug": "my_super_agenda_11",
+			"address": fullAddress,
+			"phone": fullPhone
+		}
+		
+		fetch("https://assets.breatheco.de/apis/fake/contact/", {
+				method: "POST",
+				body: JSON.stringify(contact),
+				headers: {
+					"Content-Type": "application/json"
+				}
+				})
+				.then(resp => {
+					console.log(resp.ok); // will be true if the response is successfull
+					console.log(resp.status); // the status code = 200 or code = 400 etc.
+					console.log(resp); // will try return the exact result as string
+					return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+				})
+				.then(data => {
+					//here is where your code should start after the fetch finishes
+					console.log(data); //this will print on the console the exact object received from the server
+					alert("Successfully added new contact!!")
+					// setAddress('');
+					// setEmail('');
+					// setFullName('');
+					// setPhone('');
+				})
+				.catch(error => {
+					//error handling
+					console.log(error);
+				});
+	}
+
+	
+
 	const params = useParams();
 	return (
 <div className="container">
 			<div className="row">
 				<h1 className="d-flex justify-content-center">Add a New Contact</h1>
 				<form id="frm1" action="/action_page.php" >
-				<div class="mb-3">
-  				<label for="basic-url" class="form-label">Full Name</label>
-  				<div class="input-group">
-				<input onClick={(e) => setFullName(e.target.value)} type="text" class="form-control" placeholder="Full Name" aria-label="Username" aria-describedby="basic-addon1" />
+				<div className="mb-3">
+  				<label htmlFor="basic-url" className="form-label">Full Name</label>
+  				<div className="input-group">
+				<input
+                    type="text"
+                    className="form-control"
+                    id="fullname"
+                    placeholder="insert fullname"
+                    value={fullName}
+                    onChange={(e) => setFullName(e.target.value)}
+                />
 				</div>
 				</div>
-				<div class="mb-3">
-  				<label for="basic-url" class="form-label">Email</label>
-  				<div class="input-group">
-				<input onClick={(e) => setFullEmail(e.target.value)}type="text" class="form-control" placeholder="Enter Email" aria-label="Username" aria-describedby="basic-addon1" />
+				<div className="mb-3">
+  				<label htmlFor="basic-url" className="form-label">Email</label>
+  				<div className="input-group">
+				  <input
+                    type="text"
+                    className="form-control"
+                    id="fullEmail"
+                    placeholder="insert fullname"
+                    value={fullEmail}
+                    onChange={(e) => setFullEmail(e.target.value)}
+                />
 				</div>
 				</div>				
-				<div class="mb-3">
-  				<label for="basic-url" class="form-label">Phone</label>
-  				<div class="input-group">
-				<input onClick={(e) => setFullPhone(e.target.value)}type="text" class="form-control" placeholder="Enter Phone" aria-label="Enter Phone" aria-describedby="basic-addon1" />
+				<div className="mb-3">
+  				<label htmlFor="basic-url" className="form-label">Phone</label>
+  				<div className="input-group">
+				  <input
+                    type="text"
+                    className="form-control"
+                    id="fullPhone"
+                    placeholder="insert fullname"
+                    value={fullPhone}
+                    onChange={(e) => setFullPhone(e.target.value)}
+                />				</div>
+				</div>
+				<div className="mb-3">
+  				<label htmlFor="basic-url" className="form-label">Address</label>
+  				<div className="input-group">
+				  <input
+                    type="text"
+                    className="form-control"
+                    id="fullAddress"
+                    placeholder="insert fullname"
+                    value={fullAddress}
+                    onChange={(e) => setFullAddress(e.target.value)}
+                />
 				</div>
 				</div>
-				<div class="mb-3">
-  				<label for="basic-url" class="form-label">Address</label>
-  				<div class="input-group">
-				<input onClick={(e) => setFullAddress(e.target.value)}type="text" class="form-control" placeholder="Enter Address" aria-label="Username" aria-describedby="basic-addon1" />
-				</div>
-				</div>
-				<div class="d-grid gap-2">
-				<input className="btn btn-primary" type="button" onclick="myFunction()" value="save" />
+				<div className="d-grid gap-2">
+				<button className="btn btn-primary" type="button" onClick={handleSubmit} value="save"> submit </button>
 				</div>
 				</form>
 				</div>
